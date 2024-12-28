0fab55e71168b252e37cbf802704a4d89824e7c6
kinchiller <kingchiller@gmail.com>
2024-12-28 16:09:33 +0100

    Add header component

---
+++ src/lib/components/ui/header/header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Moon from 'lucide-svelte/icons/moon';
+	import Sun from 'lucide-svelte/icons/sun';
+	import { toggleMode } from 'mode-watcher';
+
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<header class="h-16">
+	<div class="fixed left-0 top-0 h-16 w-full bg-background">
+		<div class="container flex h-full items-center">
+			<div class="grid grid-flow-col gap-2">
+				<Button href="/signup">Sign up</Button>
+				<Button href="/signin">Sign in</Button>
+
+				<Button on:click={toggleMode} variant="outline" size="icon">
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</div>
+		</div>
+	</div>
+	<slot />
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,28 +2,16 @@
 	import '../app.css';
 
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import Moon from 'lucide-svelte/icons/moon';
-	import Sun from 'lucide-svelte/icons/sun';
 	import { ModeWatcher } from 'mode-watcher';
-	import { toggleMode } from 'mode-watcher';
 
-	import { Button } from '$lib/components/ui/button/index.js';
+	import Header from '$lib/components/ui/header/header.svelte';
 	import { i18n } from '$lib/i18n';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	<div class="container">
-		<Button on:click={toggleMode} variant="outline" size="icon">
-			<Sun
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-			/>
-			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</Button>
-
+	<Header />
+	<div class="container py-8">
 		{@render children()}
 	</div>
 </ParaglideJS>
