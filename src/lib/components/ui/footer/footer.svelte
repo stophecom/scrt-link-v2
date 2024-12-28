666d0d91f55c31ce99631689a90614543a91b5a1
kinchiller <kingchiller@gmail.com>
2024-12-28 21:50:23 +0100

    Improve footer, cleanup

--- src/lib/components/ui/footer/footer.svelte
+++ src/lib/components/ui/footer/footer.svelte
@@ -5,16 +5,39 @@
 	import * as m from '$lib/paraglide/messages.js';
 	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
 
+	import Separator from '../separator/separator.svelte';
+
 	function switchToLanguage(newLanguage: AvailableLanguageTag) {
 		const canonicalPath = i18n.route(page.url.pathname);
 		const localizedPath = i18n.resolveRoute(canonicalPath, newLanguage);
 		goto(localizedPath);
 	}
+
+	import { availableLanguageTags } from '$lib/paraglide/runtime';
+	import { languageTag } from '$lib/paraglide/runtime';
+	import { cn } from '$lib/utils';
+
+	let numberOfSecrets = 124; // Make dynamic
 </script>
 
-<div>
-	<button onclick={() => switchToLanguage('en')}>en</button>
-	<button onclick={() => switchToLanguage('de')}>de</button>
-	<button onclick={() => switchToLanguage('fr')}>fr</button>
-	<button onclick={() => switchToLanguage('sr')}>sr</button>
-</div>
+<footer>
+	<div class="container flex flex-col items-center justify-center">
+		<Separator />
+		<div class="p-5 text-center">
+			{m.equal_tiny_wren_skip({ number: numberOfSecrets })}
+		</div>
+		<div class="max-w-screen-sm p-5 text-center text-sm">
+			{m.candid_red_lynx_offer()}
+		</div>
+		<div>
+			{#each availableLanguageTags as tag}
+				<button
+					class={cn('p-5 uppercase hover:text-primary', languageTag() === tag && 'text-primary')}
+					onclick={() => switchToLanguage(tag)}
+				>
+					{tag}
+				</button>
+			{/each}
+		</div>
+	</div>
+</footer>
