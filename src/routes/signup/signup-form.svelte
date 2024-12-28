39e97357ec91a7b1547621d2e306868d67bd8ed7
kinchiller <kingchiller@gmail.com>
2024-12-28 15:57:07 +0100

    Improve error message

--- src/routes/signup/signup-form.svelte
+++ src/routes/signup/signup-form.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import CircleAlert from 'lucide-svelte/icons/circle-alert';
 	import SuperDebug, { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 
 	import { dev } from '$app/environment';
 	import { page } from '$app/state';
+	import * as Alert from '$lib/components/ui/alert';
 	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
 	import * as Form from '$lib/components/ui/form';
 	import { Input } from '$lib/components/ui/input';
@@ -94,10 +96,18 @@
 			<Form.Button size="lg">Sign up</Form.Button>
 		</div>
 
+		<!-- Global error messages -->
 		{#if $message}
-			<div class="message text-red-700">{page.status} - {$message}</div>
+			<div class="py-3">
+				<Alert.Root variant="destructive">
+					<CircleAlert class="h-4 w-4" />
+					<Alert.Title>Es ist ein Fehler aufgetreten - {page.status}</Alert.Title>
+					<Alert.Description>{$message}</Alert.Description>
+				</Alert.Root>
+			</div>
 		{/if}
 
+		<!-- For debugging -->
 		{#if dev}
 			<div class="py-3">
 				<SuperDebug data={$formData} />
