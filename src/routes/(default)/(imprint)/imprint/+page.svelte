bfd025e8d17e870b6bc92f8336c5dc6c470be7a8
kinchiller <kingchiller@gmail.com>
2025-01-07 18:04:30 +0100

    Add imprint

---
+++ src/routes/(default)/(imprint)/imprint/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Page from '$lib/components/layout/page/page.svelte';
+</script>
+
+<Page title="Imprint" lead="Tl;dr: Limited liability." markNotTranslated={true}>Todo</Page>
