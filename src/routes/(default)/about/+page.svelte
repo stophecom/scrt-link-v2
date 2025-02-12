d9f31e29c21ce92828b544f323d10d4e239bc75f
kinchiller <kingchiller@gmail.com>
2025-02-12 22:46:57 +0100

    Add empty about page

---
+++ src/routes/(default)/about/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Page from '$lib/components/layout/page/page.svelte';
+</script>
+
+<Page
+	title="About"
+	lead="The whole project is based on a simple premise: The less we know, the better.">// todo</Page
+>
