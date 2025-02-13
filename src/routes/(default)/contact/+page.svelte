451520c4a264177b7e107f2f161b13a14b76e537
kinchiller <kingchiller@gmail.com>
2025-02-13 16:55:25 +0100

    Add Security

--- src/routes/(default)/contact/+page.svelte
+++ src/routes/(default)/contact/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import Page from '$lib/components/layout/page/page.svelte';
+	import { emailSupport } from '$lib/data/app';
 </script>
 
 <Page title="Contact" lead="Do you have a question or need support? Get in touch!">
 	<div class="prose">
-		<a href="mailto:info@santihans.com">info@santihans.com</a>
+		<a href="mailto:{emailSupport}">{emailSupport}</a>
 	</div>
 </Page>
--- src/routes/(default)/security/+page.svelte
+++ src/routes/(default)/security/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import Page from '$lib/components/layout/page/page.svelte';
+	import Markdown from '$lib/components/ui/markdown';
+	import * as m from '$lib/paraglide/messages.js';
 </script>
 
-<Page title="Security" lead="Best practices, no extras.">// todo</Page>
+<Page title="Security" lead="Best practices, no extras.">
+	<Markdown markdown={m.actual_patient_giraffe_rest()} format={true} />
+</Page>
