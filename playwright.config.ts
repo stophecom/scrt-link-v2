987365ff6c56950b90fe01db96f3d20aa6c8fb41
kinchiller <kingchiller@gmail.com>
2025-02-15 20:13:19 +0100

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,37 @@
-import { defineConfig } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
 
-export default defineConfig({
+// Run local tests headed: PUBLIC_ENV=development npm run test -- --headed
+const isLocalTest = process.env.PUBLIC_ENV === 'development';
+
+// Runs in local webserver
+const config: PlaywrightTestConfig = {
+	use: {
+		contextOptions: {
+			permissions: ['clipboard-read', 'clipboard-write']
+		},
+		video: 'off',
+		screenshot: 'only-on-failure'
+	},
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'VERCEL_URL=http://localhost:4173 pnpm run build && pnpm run preview',
 		port: 4173
+	},
+	testDir: 'e2e'
+};
+
+// Runs on published websites
+const configPublished: PlaywrightTestConfig = {
+	timeout: 10000,
+	use: {
+		contextOptions: {
+			permissions: ['clipboard-read', 'clipboard-write']
+		},
+		video: 'off',
+		screenshot: 'only-on-failure',
+		baseURL: process.env.VERCEL_URL || 'http://localhost:4173'
 	},
 
 	testDir: 'e2e'
-});
+};
+
+export default isLocalTest ? config : configPublished;
