39bfe8764719c11a28d72462bb2e0cf082ff4c2b
kinchiller <kingchiller@gmail.com>
2025-02-15 07:30:20 +0100

    Enable backwards compatibility

---
+++ src/routes/l/+page.svelte
@@ -0,0 +1,19 @@
+<!-- 
+This page is solely for backwards compatibility. 
+We redirect to the old version of scrt.link now living under v1.scrt.link 
+-->
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		try {
+			const redirectUrl = new URL('https://v1.scrt.link/l'); // Where the legacy version lives
+			const originalUrl = new URL(window.location.href);
+
+			redirectUrl.hash = originalUrl.hash; // Append original hash
+			window.location.replace(redirectUrl.toString());
+		} catch (e) {
+			console.error(e);
+		}
+	});
+</script>
