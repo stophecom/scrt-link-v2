98e6b0bf5b23f4caeab20e0cece9286e64d84e70
kinchiller <kingchiller@gmail.com>
2025-02-22 16:18:52 +0100

    Add contact form and ReCaptcha

---
+++ src/lib/components/forms/contact-form.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	import { PUBLIC_RECAPTCHA_CLIENT_KEY } from '$env/static/public';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import * as m from '$lib/paraglide/messages.js';
+	import { type ContactFormSchema, contactFormSchema } from '$lib/validators/formSchemas';
+
+	import Textarea from '../form-fields/textarea.svelte';
+	import FormWrapper from './form-wrapper.svelte';
+
+	let { data }: { data: SuperValidated<Infer<ContactFormSchema>> } = $props();
+
+	const form = superForm(data, {
+		validators: zodClient(contactFormSchema()),
+		validationMethod: 'auto',
+		dataType: 'json',
+
+		onSubmit: async ({ jsonData }) => {
+			const jsonPayload = $formData;
+			let recaptchaToken;
+
+			try {
+				// eslint-disable-next-line no-undef
+				await grecaptcha
+					.execute(PUBLIC_RECAPTCHA_CLIENT_KEY, { action: 'contact_form' })
+					.then(function (token) {
+						recaptchaToken = token;
+					});
+			} catch (error) {
+				console.log(error);
+			}
+
+			jsonData({ ...jsonPayload, recaptchaToken: recaptchaToken });
+		},
+
+		onError({ result }) {
+			// We use message for unexpected errors
+			$message = {
+				status: 'error',
+				title: 'Unexpected error',
+				description: result.error.message || 'Some error'
+			};
+		}
+	});
+
+	const { form: formData, message, delayed, constraints, enhance } = form;
+</script>
+
+<svelte:head>
+	<script
+		src="https://www.google.com/recaptcha/api.js?render={PUBLIC_RECAPTCHA_CLIENT_KEY}"
+	></script>
+</svelte:head>
+
+<FormWrapper message={$message}>
+	<form method="POST" action="?/contact" use:enhance>
+		<Form.Field {form} name="email" class="py-4">
+			<Form.Control let:attrs>
+				<Form.Label>{m.clear_lost_goose_beam()}</Form.Label>
+				<Input {...attrs} bind:value={$formData.email} {...$constraints.email} />
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
+		<Form.Field {form} name="content" class="flex min-h-32 flex-col justify-center">
+			<Textarea
+				bind:value={$formData.content}
+				label={m.calm_low_turkey_explore()}
+				{...$constraints.content}
+			/>
+		</Form.Field>
+
+		<div class="py-4">
+			<Form.Button delayed={$delayed} class="w-full" size="lg"
+				>{m.mild_best_ape_smile()}</Form.Button
+			>
+		</div>
+	</form>
+</FormWrapper>
---
+++ src/lib/emails/email-contact.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Body, Container, Head, Heading, Hr, Html, Img, Link, Text } from 'svelte-email-tailwind';
+
+	import { getBaseUrl } from '$lib/constants';
+	import { languageTag } from '$lib/paraglide/runtime.js';
+
+	type Props = {
+		message: string;
+		email: string;
+	};
+
+	let { message = 'Empty', email }: Props = $props();
+</script>
+
+<Html lang={languageTag()} class="font-sans">
+	<Head />
+	<Body class="bg-background ">
+		<Container class="py-12">
+			<Img src={`${getBaseUrl()}/logo.png`} alt="Logo" width="140" height="140" />
+
+			<Heading class="text-primary text-4xl ">Contact</Heading>
+
+			<Text class="mb-10 leading-snug">Message from: {email}</Text>
+			<Text class="mb-10 leading-snug">{message}</Text>
+
+			<Hr class="border-border mt-8" />
+
+			<Text class="text-muted text-xs">
+				©{new Date().getFullYear()} scrt.link -
+				<Link class="text-muted" href="{getBaseUrl()}/privacy-policy">Privacy Policy</Link>
+			</Text>
+		</Container>
+	</Body>
+</Html>
