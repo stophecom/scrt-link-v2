2e565105ead36db59d2c9f70f63899d85a5e2cbb
kinchiller <kingchiller@gmail.com>
2025-03-01 21:02:03 +0100

    Add url explanation section

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import ArrowRight from 'lucide-svelte/icons/arrow-right';
+
 	import Burn from '$lib/assets/images/illustrations/burn.svg?component';
 	import Write from '$lib/assets/images/illustrations/create.svg?component';
 	import Share from '$lib/assets/images/illustrations/share.svg?component';
+	import UrlExplained from '$lib/assets/images/illustrations/url-explained.svg?component';
 	import IntersectionObserver from '$lib/components/helpers/intersection-observer.svelte';
 	import CreateSecret from '$lib/components/layout/create-secret.svelte';
 	import Page from '$lib/components/layout/page/page.svelte';
@@ -42,12 +45,12 @@
 	iterator: number
 )}
 	<Card
-		class="grid grid-cols-[33%_1fr] grid-rows-2 gap-2 px-4! !py-0 ps-0! sm:grid-cols-none sm:grid-rows-none sm:py-6! sm:ps-4! sm:text-center"
+		class="grid grid-cols-[33%_1fr] grid-rows-2 gap-2 px-4! !py-0 ps-0! sm:grid-cols-none sm:grid-rows-[min-content_min-content_1fr]  sm:py-6! sm:ps-4! sm:text-center"
 	>
 		<h4 class="xs:text-2xl order-2 self-end text-xl font-bold sm:order-1 sm:mb-4 sm:text-3xl">
 			{iterator + 1}. {title}
 		</h4>
-		<Illustration class="order-1 row-span-2 sm:order-2" />
+		<Illustration class="order-1 row-span-2 sm:order-2 sm:row-span-1" />
 		<p class="xs:text-sm order-3 text-xs font-semibold sm:order-3">
 			{description}
 		</p>
@@ -64,7 +67,7 @@
 				{#each explanationSteps() as step, i}
 					<div
 						style="transition-delay: {i * 100}ms;"
-						class="transition-all {intersecting
+						class="flex transition-all {intersecting
 							? 'translate-y-0 scale-100 opacity-100 duration-700'
 							: 'translate-y-20 scale-90 opacity-0'}"
 					>
@@ -73,6 +76,24 @@
 				{/each}
 			</div>
 		</IntersectionObserver>
+	</Section>
+
+	<Section title={m.flat_zany_baboon_adapt()} lead={m.royal_odd_ox_read()}>
+		<IntersectionObserver let:intersecting top={-100} once={true}>
+			<div
+				class="flex transition-all {intersecting
+					? 'translate-y-0 opacity-100 duration-500'
+					: 'translate-y-10 opacity-0'}"
+			>
+				<Card class="mb-2">
+					<UrlExplained />
+					{m.equal_elegant_herring_yell()}
+				</Card>
+			</div>
+		</IntersectionObserver>
+		<Button variant="ghost" href="/security"
+			>{m.happy_plain_panther_fry()}<ArrowRight class="ms-2 h-4 w-4" /></Button
+		>
 	</Section>
 
 	<Section title="FAQ" lead={m.stock_keen_marten_commend()}>
