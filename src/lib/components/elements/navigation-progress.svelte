19b67a28a32d088cac8ea3c6e4532ef550eeb273
kinchiller <kingchiller@gmail.com>
2025-03-13 21:57:41 +0100

    Fix NavigationProgress visibility

--- src/lib/components/elements/navigation-progress.svelte
+++ src/lib/components/elements/navigation-progress.svelte
@@ -2,6 +2,7 @@
 	import { navigating } from '$app/state';
 
 	let current = $state(0);
+	let showProgress = $state(false);
 	let interval: ReturnType<typeof setInterval>;
 	const max = 95;
 	const matrix = [
@@ -22,6 +23,7 @@
 		if (interval) {
 			return;
 		}
+		showProgress = true;
 		current = 0;
 		interval = setInterval(inc, 300);
 	}
@@ -29,6 +31,7 @@
 	function done() {
 		clearInterval(interval);
 		current = 99;
+		showProgress = false;
 	}
 
 	$effect(() => {
@@ -44,9 +47,9 @@
 </script>
 
 <div
-	class="progress bg-primary fixed top-0 left-0 z-50 h-[2px] w-full opacity-100 transition-all {navigating.to
-		? 'opacity-0'
-		: 'opacity-100'}"
+	class="progress bg-primary fixed top-0 left-0 z-50 h-[2px] w-full transition-all {showProgress
+		? 'opacity-100'
+		: 'opacity-0'}"
 	style="--progress: {current}%"
 ></div>
 
