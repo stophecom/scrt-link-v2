c84a9dc53e5c9969104ccba00faba25907a9d90c
kinchiller <kingchiller@gmail.com>
2025-03-17 21:43:59 +0100

    Add spanish

--- src/lib/data/supportedLocales.ts
+++ src/lib/data/supportedLocales.ts
@@ -1,9 +1,11 @@
 import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
 
+// Keep in sync with project.inlang/settings.json
 const map: Record<AvailableLanguageTag, string> = {
 	de: 'Deutsch',
 	en: 'English',
-	fr: 'Français'
+	fr: 'Français',
+	es: 'Español'
 };
 
 export const getSupportedLanguagesMap = (locale: AvailableLanguageTag) => {
