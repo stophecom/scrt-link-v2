1119b4e328e12af84c8aa5e47ff8d31be8f4fc59
kinchiller <kingchiller@gmail.com>
2025-03-25 20:17:35 +0100

    Fix CORS

--- src/routes/api/v1/secrets/+server.ts
+++ src/routes/api/v1/secrets/+server.ts
@@ -15,7 +15,7 @@ export async function OPTIONS() {
 		headers: {
 			'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Methods': 'POST, OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type'
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Checksum'
 		}
 	});
 }
