6a6dcbaac937643e667cfff186d2c28308ec54b8
kinchiller <kingchiller@gmail.com>
2025-04-23 20:48:09 +0200

    Move default error template

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -3,6 +3,6 @@
 	import Page from '$lib/components/page/page.svelte';
 </script>
 
-<Page title={`${page.status}`} lead={page.error?.message || 'Unexpted error.'}>
+<Page title={`${page.status}`} lead={page.error?.message || 'Unexpected error.'}>
 	<span class="text-9xl">😥</span>
 </Page>
