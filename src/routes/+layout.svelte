54437869fbdae32f79d87c0148d16044c00f980b
kinchiller <kingchiller@gmail.com>
2025-04-24 22:31:05 +0200

    Remove NavigationProgress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,8 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import type { Snippet } from 'svelte';
 
-	import NavigationProgress from '$lib/components/elements/navigation-progress.svelte';
-
 	let { children }: { children: Snippet } = $props();
 </script>
-
-<NavigationProgress />
 
 {@render children()}
 
