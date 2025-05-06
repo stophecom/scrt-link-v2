57fdfa36345f92fd6652cf3da004a9e33e714274
kinchiller <kingchiller@gmail.com>
2025-05-06 22:28:56 +0200

    Add table component

---
+++ src/lib/components/ui/table/table-cell.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { HTMLTdAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/client/utils';
+
+	type $$Props = HTMLTdAttributes;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<td
+	class={cn('px-2 py-1 align-middle [&:has([role=checkbox])]:pr-0', className)}
+	{...$$restProps}
+	on:click
+	on:keydown
+>
+	<slot />
+</td>
